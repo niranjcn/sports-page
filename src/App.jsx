0f774cc8a815963a5a52f72a033b51b419e9a71a
Niranj <niranjcn024@gmail.com>
2024-12-21 20:32:39 +0530

    change2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import  Nav  from './components/Nav.jsx'
+import Slider from './components/Slider.jsx'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <Nav />
+    <Slider />
     </>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,19 +5,19 @@ const Nav = () => {
   return (
     <div>
      <nav className="h-20 w-full bg-white flex items-center justify-between px-6">
-        <div className=''>
+        <div className='ml-5'>
         <img src={stmlogo} alt="St Thomas Kannur" className="h-16 w-auto mt-2" />
         </div>
         <div>
         
-        <div className="space-x-6 flex">
-          <a href="#" className="text-black">HOME</a>
-          <a href="#" className="text-black">SCORE</a>
-          <a href="#" className="text-black">EVENTS</a>
-          <a href="#" className="text-black">GALLERY</a>
-          <a href="#" className="text-black">TEAMS</a>
-          <a href="#" className="text-black">STATISTICS</a>
-          <a href="#" className="text-black">REGISTER NOW</a>
+        <div className="space-x-6 flex mr-14 text-sm font-semibold text-cyan-700">
+          <a href="#" className=" hover:text-emerald-800">HOME</a>
+          <a href="#" className=" hover:text-emerald-800">SCORE</a>
+          <a href="#" className=" hover:text-emerald-800">EVENTS</a>
+          <a href="#" className=" hover:text-emerald-800">GALLERY</a>
+          <a href="#" className=" hover:text-emerald-800">TEAMS</a>
+          <a href="#" className=" hover:text-emerald-800">STATISTICS</a>
+          <a href="#" className=" hover:text-emerald-800">REGISTER NOW</a>
         </div>
             
         </div>
