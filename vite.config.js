4dabf3f58798d87590ec77ee037c65867a630ab2
Niranj <niranjcn024@gmail.com>
2025-01-11 11:15:45 +0530

    change17

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensure the entry point is correct
+    },
+  },
+});
